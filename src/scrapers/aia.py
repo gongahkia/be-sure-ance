6895f4011954a23f6577033dfdf111904f971f5d
gongahkia <angryapplegravy@gmail.com>
2025-03-08 16:56:35 +0800

    feat(app): doned

--- src/scrapers/aia.py
+++ src/scrapers/aia.py
@@ -13,11 +13,60 @@ https://www.aia.com.sg/en/our-products/save-and-invest
 
 import os
 import asyncio
-import lib.sanitise
+
+# import lib.sanitise
 from playwright.async_api import async_playwright
 import json
 
 # ----- functions -----
+
+
+def remove_excess_newlines(inp):
+    if not isinstance(inp, str):
+        raise TypeError(
+            f"Input must be type <string> but was type <{type(inp).__name__}>"
+        )
+    inp = re.sub(r"\n+", "\n", inp)
+    inp = re.sub(r"[ \t\u200b]+", " ", inp)
+    return inp.strip()
+
+
+def remove_html_entities(inp):
+    inp2 = html.unescape(inp)
+    replacements = {
+        "\xa0": " ",  # Non-breaking space
+        "\u200b": "",  # Zero-width space
+        "\u2013": "-",  # En dash
+        "\u2014": "--",  # Em dash
+        "\u2026": "...",  # Ellipsis
+        "\u2018": "'",  # Left single quote
+        "\u2019": "'",  # Right single quote
+        "\u201c": '"',  # Left double quote
+        "\u201d": '"',  # Right double quote
+        "\u00ab": '"',  # Left guillemet
+        "\u00bb": '"',  # Right guillemet
+        "\u02c6": "^",  # Circumflex
+        "\u2039": "<",  # Single left angle quote
+        "\u203a": ">",  # Single right angle quote
+        "\u02dc": "~",  # Small tilde
+        "\u00a9": "(c)",  # Copyright symbol
+        "\u00ae": "(R)",  # Registered trademark symbol
+        "\u2122": "(TM)",  # Trademark symbol
+        "\u00b0": "°",  # Degree symbol
+        "\u00b7": "*",  # Middle dot
+        "\u00b1": "+/-",  # Plus-minus symbol
+        "\u00bc": "1/4",  # One-quarter fraction
+        "\u00bd": "1/2",  # One-half fraction
+        "\u00be": "3/4",  # Three-quarters fraction
+        "&lt;": "<",  # Less-than sign (HTML entity)
+        "&gt;": ">",  # Greater-than sign (HTML entity)
+        "&amp;": "&",  # Ampersand (HTML entity)
+        "&quot;": '"',  # Quotation mark (HTML entity)
+        "&apos;": "'",  # Apostrophe (HTML entity)
+    }
+    for old_char, new_char in replacements.items():
+        inp2 = inp2.replace(old_char, new_char)
+    return inp2
 
 
 async def scrape_data(target_url):
@@ -157,5 +206,6 @@ scrape_list = [
 script_dir = os.path.dirname(os.path.abspath(__file__))
 scraped_dir = os.path.join(os.path.dirname(script_dir), "scraped")
 json_filepath = os.path.join(scraped_dir, "aia.json")
+print(json_filepath)
 
 asyncio.run(main(json_filepath, scrape_list))
