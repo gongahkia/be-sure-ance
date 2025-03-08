59256a0adbf9ac164af6aef9b5deed140f6726f2
gongahkia <angryapplegravy@gmail.com>
2025-03-08 16:23:00 +0800

    feat(app): added

--- src/scrapers/uoi.py
+++ src/scrapers/uoi.py
@@ -29,7 +29,7 @@ async def scrape_data(url):
     plan_benefits = []
 
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         context = await browser.new_context()
         page = await context.new_page()
         await page.goto(url)
