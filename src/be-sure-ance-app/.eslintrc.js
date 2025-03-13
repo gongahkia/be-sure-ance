2ec3d8782f762ef07a010ea4b2cd4179a8a124ab
gongahkia <angryapplegravy@gmail.com>
2025-03-13 12:02:34 +0800

    feat(app): added

--- src/be-sure-ance-app/.eslintrc.js
+++ src/be-sure-ance-app/.eslintrc.js
@@ -1,5 +1,21 @@
 module.exports = {
+  root: true,
   env: {
-    "vue/setup-compiler-macros": true,
+    node: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential', 
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue', 
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
   },
 };
